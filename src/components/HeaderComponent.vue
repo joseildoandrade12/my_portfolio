7e9652a4e161e2d16761b0260b5e3c7e1f5d3698
Joseildo <joseildoandrade1405@gmail.com>
2026-02-14 19:56:01 -0300

    feat:create component Menu Header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,1 +1,99 @@
-<template></template>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { RouterLink } from 'vue-router';
+  import HomeIcon from './icons/HomeIcon.vue';
+  import MenuIcon from './icons/MenuIcon.vue';
+  import ThemePage from './icons/MoonIcon.vue';
+  import MenuComponent from './MenuComponent.vue';
+
+  const activeMenu = ref(false);
+</script>
+
+<template>
+  <div class="container-header">
+    <RouterLink to="/">
+      <HomeIcon class="home-icon" fill="#63B3FF" />
+      <h1>JoseildoDev</h1>
+    </RouterLink>
+    <MenuComponent class="itens-menu " v-if="activeMenu"/>
+    <nav>
+      <button>
+        <ThemePage fill="#ffffffb3" width="20px" height="20px" />
+      </button>
+      <button @click="activeMenu = !activeMenu">
+        <MenuIcon fill="#ffffffb3" width="20px" height="20px" />
+      </button>
+    </nav>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  %standard-button-hover {
+    cursor: pointer;
+    padding: 0.625rem;
+    border-radius: 0.5rem;
+    &:hover {
+      background-color: $btn-secondary-hover;
+    }
+  }
+
+  .container-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.625rem 1.5rem;
+    border-radius: 0 0 0.75rem 0.75rem;
+    background-color: $bg-sections;
+
+    a {
+      display: flex;
+      align-items: center;
+      transition: all 300ms ease-in-out;
+      @extend %standard-button-hover;
+
+      h1 {
+        color: $primary-color;
+        padding-top: 4px;
+        margin-left: 0.5rem;
+        font-weight: bold;
+        font-size: 0.875rem;
+      }
+    }
+
+    .itens-menu {
+      animation: fadeIn 300ms ease-in-out;
+      position: absolute;
+      top: 80px;
+      right: 24px;
+
+      &.active {
+        top: 0;
+        position: relative;
+      }
+    }
+
+    nav {
+      display: flex;
+      align-items: center;
+      gap: 1.5rem;
+
+      button {
+        background-color: transparent;
+        border: none;
+        transition: all 300ms ease-in-out;
+        @extend %standard-button-hover;
+      }
+    }
+
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+        transform: translateY(-10px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+  }
+</style>
