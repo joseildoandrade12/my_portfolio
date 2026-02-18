2ef23567a811029faf7d8b6a0c22dad2ba5777c5
joseildoandrade12 <joseildoandrade1405@gmail.com>
2026-02-17 21:48:17 -0300

    feat: addition background image

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 </script>
 
 <template>
-  <HeaderComponent />
+  <div class="container-image"><img src="../public/sl_031420_28950_10.jpg" alt=""></div>
+  <HeaderComponent class="menu"/>
   <main>
     <RouterView />
   </main>
@@ -13,9 +14,27 @@
 </template>
 
 <style scoped lang="scss">
+  .container-image {
+    width: 100vw;
+    height: 320px;
+    z-index: -999;
+    position: absolute;
+    left: 0;
+    top: 0;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .menu {
+    margin-bottom: 180px;
+  }
   main {
     padding: 1.5rem;
     border-radius: 0.75rem;
     background-color: $bg-sections;
+    margin-bottom: 24px;
   }
 </style>
