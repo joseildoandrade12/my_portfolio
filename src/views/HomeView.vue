ffc507647b3ed87bd6c030c5de2c051c534ccb08
joseildoandrade12 <joseildoandrade1405@gmail.com>
2026-02-22 01:52:35 -0300

    feat: using component postItem and addition style and JS with menu of navigation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,113 @@
-<template></template>
+<script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import PostItem from '@/components/PostItem.vue';
+  import ArrowIcon from '@/components/icons/ArrowIcon.vue';
+
+  import { useDataPost } from '@/composables/useDataPost';
+  import type { DataPost } from '@/types/types';
+
+  const { dataPost } = useDataPost();
+  const arraySeparatingPosts = ref<DataPost[][]>([]);
+  const countSectionPost = ref(0);
+
+  function separatingArrays(arr: DataPost[]) {
+    arraySeparatingPosts.value = [];
+
+    for (let i = 0; i < arr.length; i += 3) {
+      arraySeparatingPosts.value.push(arr.slice(i, i + 3));
+    }
+  }
+
+  function subtractCountSectionPost() {
+    if (countSectionPost.value <= 0) {
+      return;
+    }
+    countSectionPost.value--;
+  }
+
+  function sumCountSectionPost() {
+    if (countSectionPost.value + 1 == arraySeparatingPosts.value.length) {
+      return;
+    }
+    countSectionPost.value++;
+  }
+
+  onMounted(() => {
+    separatingArrays(dataPost.value.reverse());
+  });
+</script>
+
+<template>
+  <div class="container-home">
+    <PostItem
+      v-for="post in arraySeparatingPosts[countSectionPost]"
+      :key="post.id"
+      class="post-item"
+      :post="post" />
+    <div class="container-navegation">
+      <button @click="subtractCountSectionPost">
+        <ArrowIcon fill="#181E24" />
+      </button>
+      <p>{{ countSectionPost + 1 }}</p>
+      <button @click="sumCountSectionPost">
+        <ArrowIcon fill="#181E24" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  $color-line: $btn-regular;
+  .container-home {
+    min-height: 490px;
+
+    .post-item {
+      margin-bottom: 1rem;
+      padding-bottom: 1rem;
+      border-bottom: 1px dashed $color-line;
+      cursor: pointer;
+    }
+
+    .container-navegation {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 1.5rem;
+
+      button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 36px;
+        height: 36px;
+        border: none;
+        border-radius: 0.375rem;
+        background-color: $bg-principal;
+        cursor: pointer;
+        transition: 200ms ease-in-out;
+
+        &:hover {
+          background-color: $primary-color;
+        }
+      }
+
+      p {
+        color: $bg-sections;
+        font-weight: bold;
+        width: 36px;
+        height: 36px;
+        background-color: $primary-color;
+        border-radius: 0.375rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      &:last-child {
+        button:last-child {
+          transform: rotate(180deg);
+        }
+      }
+    }
+  }
+</style>
